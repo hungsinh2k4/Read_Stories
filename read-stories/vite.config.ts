35a8b7b3d506d19991346867b58309aaf7273fa4
Nguyễn Sinh Hùng <117707758+hungsinh2k4@users.noreply.github.com>
2025-10-01 19:19:39 +0700

    update vite

--- read-stories/vite.config.ts
+++ read-stories/vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  // load biến từ file .env
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react(), tailwindcss()],
-    base: env.VITE_TRUYEN_TRANH_VIP || '/Read-Stories',
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_TRUYEN_TRANH_VIP || '/Read-Stories',
 })
