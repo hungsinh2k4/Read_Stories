532344ab743cc56b87d70354c96838ce8e8e9999
Nguyễn Sinh Hùng <117707758+hungsinh2k4@users.noreply.github.com>
2025-10-03 16:27:14 +0700

    FIX

--- read-stories/src/pages/NewStories.tsx
+++ read-stories/src/pages/NewStories.tsx
@@ -67,7 +67,7 @@ const NewStories: React.FC<NewStoriesProps> = ({ title }) => {
       <StoriesSection
         stories={stories}
         cdnDomain={cdnDomain}
-        title={title || "TRUYỆN ĐÃ HOÀN THÀNH"}
+        title={title || "TRUYỆN MỚI CẬP NHẬT"}
         columnsClassName="grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6"
       />
 
